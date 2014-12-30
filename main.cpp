51b4ab210c1ff9d48a5d73e4117602bd1afeb174
Brian Uosseph <brian.uosseph@gmail.com>
2014-12-30 15:34:06 -0500

    Set up input parse functions

--- main.cpp
+++ main.cpp
@@ -1,7 +1,56 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
 using namespace std;
+
+enum Input {
+	Comment = (int)'#',
+	Vertex	= (int)'v',
+	Face	= (int)'f',
+	Object	= (int)'o',
+
+};
+
+void parseComment(string line) {
+	cout << line << '\n';
+}
+
+void parseVertex(string line) {
+	cout << line << '\n';
+}
+
+void parseFace(string line) {
+	cout << line << '\n';
+}
+
+void parseObject(string line) {
+	cout << line << '\n';
+}
+
+void parseFile(ifstream& file) {
+	string line;
+	while(getline(file, line)) {
+		Input input = (Input)(int)line[0];
+
+		switch(input) {
+			case Comment:
+				parseComment(line);
+				break;
+			case Vertex:
+				parseVertex(line);
+				break;
+			case Face:
+				parseFace(line);
+				break;
+			case Object:
+				parseObject(line);
+				break;
+			default:
+				break;
+		}
+	}
+}
 
 bool isWavefront(char* filename) {
 	if (filename[0] == '\0') {
@@ -19,10 +68,6 @@ bool isWavefront(char* filename) {
 	char* suffix = filename + i;
 	char obj[] = ".obj";
 	return (strcmp(suffix, obj) == 0);
-}
-
-void parseFile(ifstream& file) {
-	cout << "succss" << endl;
 }
 
 bool openFile(char* filename) {
