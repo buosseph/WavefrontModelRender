f4092de26727c3d50a86e415e5d186a249a0c6ec
Brian Uosseph <brian.uosseph@gmail.com>
2014-12-31 19:45:30 -0500

    Setup png encoder

--- main.cpp
+++ main.cpp
@@ -4,8 +4,9 @@
 #include <string>
 #include <vector>
 #include <stdlib.h>
+#include <png.h>
 
-#include "vertex.h"
+// #include "vertex.h"
 
 using namespace std;
 
@@ -142,6 +143,93 @@ bool openFile(char* filename) {
 	return true;
 }
 
+inline void setRGB(png_byte *ptr, float val) {
+	int v = (int)(val * 767);
+	if (v < 0) v = 0;
+	if (v > 767) v = 767;
+	int offset = v % 256;
+
+	if (v<256) {
+		ptr[0] = 0; ptr[1] = 0; ptr[2] = offset;
+	}
+	else if (v<512) {
+		ptr[0] = 0; ptr[1] = offset; ptr[2] = 255-offset;
+	}
+	else {
+		ptr[0] = offset; ptr[1] = 255-offset; ptr[2] = 0;
+	}
+}
+
+bool saveScene(char* filename, int width, int height, float* buffer, char* title) {
+	FILE *fp;
+	png_structp png_ptr;
+	png_infop info_ptr;
+	png_bytep row;
+
+	fp = fopen(filename, "wb");
+	if (fp == NULL) {
+		fprintf(stderr, "Could not open %s for writing\n", filename);
+		return false;
+	}
+
+	png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+	if (png_ptr == NULL) {
+		fprintf(stderr, "Could not allocate write struct\n");
+		// cleanup
+		return false;
+	}
+
+	info_ptr = png_create_info_struct(png_ptr);
+	if (info_ptr == NULL) {
+		fprintf(stderr, "Could not allocate info struct\n");
+		// cleanup
+		return false;
+	}
+
+	if (setjmp(png_jmpbuf(png_ptr))) {
+		fprintf(stderr, "Error during png creation\n");
+		// cleanup
+		return false;
+	}
+
+	png_init_io(png_ptr, fp);
+	// Write header (8-bit color)
+	png_set_IHDR(png_ptr, info_ptr, width, height,
+		8, PNG_COLOR_TYPE_RGB, PNG_INTERLACE_NONE,
+		PNG_COMPRESSION_TYPE_BASE, PNG_FILTER_TYPE_BASE);
+
+	if (title != NULL) {
+		png_text title_text;
+		title_text.compression = PNG_TEXT_COMPRESSION_NONE;
+		title_text.key = "Title";
+		title_text.text = title;
+		png_set_text(png_ptr, info_ptr, &title_text, 1);
+	}
+
+	png_write_info(png_ptr, info_ptr);
+
+	// Memory for one row (3 bytes per pixel - RGB)
+	row = (png_bytep) malloc(3 * width * sizeof(png_byte));
+
+	int x, y;
+	for (y = 0; y < height; y++) {
+		for (x = 0; x < width; x++) {
+			setRGB(&(row[x*3]), buffer[y*width + x]);
+		}
+		png_write_row(png_ptr, row);
+	}
+
+	png_write_end(png_ptr, NULL);
+
+	// cleanup
+	if (fp != NULL) fclose(fp);
+	if (info_ptr != NULL) png_free_data(png_ptr, info_ptr, PNG_FREE_ALL, -1);
+	if (png_ptr != NULL) png_destroy_write_struct(&png_ptr, (png_infopp)NULL);
+	if (row != NULL) free(row);
+
+	return true;
+}
+
 int main(int argc, char* argv[]) {
 	if (argc < 2) {
 		cout
@@ -151,5 +239,13 @@ int main(int argc, char* argv[]) {
 		return -1;
 	}
 	openFile(argv[1]);
+
+	char* filename = "output.png";
+	int width = 4;
+	int height = 4;
+	char* title = "Test";
+	float buffer [width*height*3];	// all 0.
+
+	cout << saveScene(filename, width, height, buffer, title) << endl;
 	return 0;	
 }
